f8f1eb869e46cdadfcf7e4d671c80bed38b1e02f
scheema <srinivas.cheema@gmail.com>
2023-03-26 19:18:11 -0400

    Added comments to make code better to understand

--- day1.py
+++ day1.py
@@ -5,10 +5,16 @@
 # 3) Combine these two to generate a band name
 
 print(f'Welcome to the Band Name Generator program!')
+
+# Ask for the place of birth
 print(f'What\'s the name of the city you group up in ?')
 place_of_birth = input()
+
+# 2) Ask for the pet name
 print(f'What\'s your pet name ?')
 pet_name = input()
+
+# Generate a band name
 print(f'Your band name could be {place_of_birth} {pet_name} ')
 
 
